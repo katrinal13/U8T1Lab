0823764f0723564008167be2109e20b826b48e93
BT_1E10_34 <sample@bths.edu>
2022-03-03 10:47:05 -0500

    updated U8T1Lab

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -14,4 +14,38 @@ public class FunWith2DArrays
         System.out.println(arr[arr.length - 1][0]);
         System.out.println(arr[arr.length - 1][arr[0].length - 1]);
     }
+
+    public static double average(int[][] arr)
+    {
+        double sum = 0;
+        int numElements = arr.length * arr[0].length;
+        for (int[] intArr : arr)
+        {
+            for (int element : intArr)
+            {
+                sum += element;
+            }
+        }
+        return sum / numElements;
+    }
+
+    public static int[] indexFound(String[][] arr, String target)
+    {
+        int[] targetFound = new int[2];
+        for (int row = 0; row < arr.length; row++)
+        {
+            for (int col = 0; col < arr[0].length; col++)
+            {
+                if(arr[row][col].indexOf(target) != -1)
+                {
+                    targetFound[0] = row;
+                    targetFound[1] = col;
+                    return targetFound;
+                }
+            }
+        }
+        targetFound[0] = -1;
+        targetFound[1] = -1;
+        return targetFound;
+    }
 }
